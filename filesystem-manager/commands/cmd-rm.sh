b95055a9473d5f5e9dbf896cd6e5d46ca902b123
zaufi <i.zaufi@gmail.com>
2012-11-05 19:16:28 +0400

    make empty dirs removal recursive

--- filesystem-manager/commands/cmd-rm.sh
+++ filesystem-manager/commands/cmd-rm.sh
@@ -3,6 +3,22 @@
 # Plugin to implement a `rm` command of the config file
 #
 
+function _remove_empty_dirs_reqursively()
+{
+    local cd="$1"
+
+    # Check if there is something else in the `cd` dir?
+    local hasSmth=`find "${D}/${cd}" ! -type d`
+    if [ -z "${hasSmth}" ]; then
+        # NO! Remove an empty dir to avoid warnings!
+        rm -rf ${D}/${cd}
+        local parent=`dirname "${cd}"`
+        if [ "$parent" != "/"]; then
+            _remove_empty_dirs_reqursively "$parent"
+        fi
+    fi
+}
+
 #
 # Function to remove smth in a given directory
 #
@@ -18,11 +34,6 @@ function cmd_rm()
       && rm -rf ${dst} 2>/dev/null \
       && cd -
     result=$?
-    # Check if there something else in a `cd` dir?
-    local hasSmth=`find "${D}/${cd}" ! -type d`
-    if [ -z "${hasSmth}" ]; then
-        # NO! Remove an empty dir to avoid warnings!
-        rm -rf ${D}/${cd}
-    fi
+    _remove_empty_dirs_reqursively "${cd}"
     eend $result
 }
