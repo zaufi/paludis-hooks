68cf7297032b60e0183a4f4ec8ae39ed46bf4cd0
zaufi <i.zaufi@gmail.com>
2013-12-30 07:28:36 +0400

    add missed file

---
+++ profile.d/00-make-completion-wrapper.sh
@@ -0,0 +1,19 @@
+#
+# Helper function to generate completion wrappers for aliased commands
+#
+
+function make-completion-wrapper()
+{
+    local function_name="$2"
+    local arg_count=$(($#-3))
+    local comp_function_name="$1"
+    shift 2
+    local function="
+function $function_name {
+    ((COMP_CWORD+=$arg_count))
+    COMP_WORDS=( "$@" \${COMP_WORDS[@]:1} )
+    "$comp_function_name"
+    return 0
+}"
+    eval "$function"
+}
