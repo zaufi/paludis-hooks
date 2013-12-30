8115ea9e1c86faa23ee5c20dd60bc4a1a337815f
zaufi <i.zaufi@gmail.com>
2013-12-30 07:27:10 +0400

    fix paths declaration order. add a way to still use completions w/ aliases

--- profile.d/cave-aliases.sh
+++ profile.d/cave-aliases.sh
@@ -14,3 +14,17 @@ alias cy='cave sync'
 alias cz='cave resume -Cs ${CAVE_RESUME_FILE_OPT}'
 alias world-up='cave resolve ${CAVE_RESUME_FILE_OPT} -c -km -Km -Cs -P "*/*" -Si -Rn world'
 alias system-up='cave resolve ${CAVE_RESUME_FILE_OPT} -c -km -Km -Cs -P "*/*" -Si -Rn system'
+
+#
+# Reuse cave bash completer to generate compltions for just introduced aliases
+#
+make-completion-wrapper _cave _cs cave search
+make-completion-wrapper _cave _cm cave manage-search-index
+make-completion-wrapper _cave _cc cave contents
+make-completion-wrapper _cave _cr cave resolve
+make-completion-wrapper _cave _crz cave resolve
+make-completion-wrapper _cave _cw cave show
+make-completion-wrapper _cave _co cave owner
+make-completion-wrapper _cave _cu cave uninstall
+make-completion-wrapper _cave _cy cave sync
+make-completion-wrapper _cave _cz cave resume
