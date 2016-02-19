1b1aae9e9b62ab83c9749bc3d400f07dee73447e
Alex Turbov <i.zaufi@gmail.com>
2016-02-19 23:53:51 +0530

    implement `mv` element of FSM hook

---
+++ filesystem-manager/commands/cmd-mv.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Plugin to implement an `mv` command of the config file
+#
+
+#
+# Function to move smth in a given directory
+#
+# @param cd  -- directory to change to, before remove
+# @param dst -- destination: directory or new file name
+# @param src... -- what to move (possible w/ wildcards)
+function cmd_mv()
+{
+    local cd="$1"
+    local dst="$2"
+    shift 2
+
+    if ! verify_dir "${cd}"; then
+        eerror "Package image dir is undefined! Skip any actions..."
+        return
+    fi
+
+    if [ -z "$*" ]; then
+        # NOTE This shouldn't happened! Config file will be validated with DTD...
+        eerror "No source to move has specified"
+        return
+    fi
+
+    if [ -d "${D}/${cd}" ]; then
+        cd "${D}/${cd}"
+        mv -vf "$@" ${dst} 2>/dev/null && schedule_a_warning_after_all
+        cd - >/dev/null
+        # Walk through whole image and try to remove possible empty dirs
+        # ATTENTION According EAPI it is incorrect to install empty directories!
+        # If a package need some, then its ebuild must use `keepdir` for this!
+        # So this action also can be considered as sanitize an image before install :)
+        find ${D} -type d -a -empty -exec rmdir -p --ignore-fail-on-non-empty {} +
+    fi
+}
